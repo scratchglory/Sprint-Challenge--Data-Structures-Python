8763e39ab202d105df4eb051eeeddfe203f47e5f
adella <scratchglory@gmail.com>
2020-06-26 21:09:35 -0400

    finished mvp

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,13 +12,16 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
 
     def add_to_head(self, value):
+        # creates a node to add
         node = Node(value)
 
+        # checks if this is empty
         if self.head is not None:
             node.set_next(self.head)
 
@@ -28,6 +31,7 @@ class LinkedList:
         if not self.head:
             return False
 
+        # traverse through all nodes until we see the value or can't go further
         current = self.head
 
         while current:
@@ -39,4 +43,19 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # print("NODE", node)  # object
+        # print("PREV", prev)  # None
+
+        # set current node
+        current = self.head
+        while current is not None:
+            # instantiate getting the next node
+            next_node = current.get_next()
+            # set the current with the previous node
+            current.set_next(prev)
+            # previous is current
+            prev = current
+            # current is the next node that was fetched
+            current = next_node
+        # the head is now the previous
+        self.head = prev
